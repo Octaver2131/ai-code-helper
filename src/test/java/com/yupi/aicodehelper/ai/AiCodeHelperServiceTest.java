76ca88b542508a3087f817040c58f7c126645bd8
Octaver <Octaver2131@outlook.com>
2025-09-20 16:01:53 +0800

    feat: 创建 AI Service

---
+++ src/test/java/com/yupi/aicodehelper/ai/AiCodeHelperServiceTest.java
@@ -0,0 +1,20 @@
+package com.yupi.aicodehelper.ai;
+
+import jakarta.annotation.Resource;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class AiCodeHelperServiceTest {
+
+    @Resource
+    private AiCodeHelperService aiCodeHelperService;
+
+    @Test
+    void chat() {
+        String result = aiCodeHelperService.chat("讲解 3D 接雨水的算法思路");
+        System.out.println(result);
+    }
+}
