0956da16b490767e419bce297800d9fc0ee81e36
Octaver <Octaver2131@outlook.com>
2025-09-20 15:14:21 +0800

    feat: 创建 chat model

--- src/test/java/com/yupi/aicodehelper/AiCodeHelperApplicationTests.java
+++ src/test/java/com/yupi/aicodehelper/AiCodeHelperApplicationTests.java
@@ -1,13 +1,18 @@
 package com.yupi.aicodehelper;
 
+import com.yupi.aicodehelper.ai.AiCodeHelper;
+import jakarta.annotation.Resource;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class AiCodeHelperApplicationTests {
 
-    @Test
-    void contextLoads() {
-    }
+    @Resource
+    private AiCodeHelper aiCodeHelper;
 
+    @Test
+    void chat() {
+        aiCodeHelper.chat("Hello, Qwen-max！");
+    }
 }
