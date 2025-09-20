53c837abb3571bb89a0c94da6b81151f14867dc1
Octaver <Octaver2131@outlook.com>
2025-09-20 16:08:16 +0800

    refactor: 不建议使用 @AiService

--- src/main/java/com/yupi/aicodehelper/ai/AiCodeHelperService.java
+++ src/main/java/com/yupi/aicodehelper/ai/AiCodeHelperService.java
@@ -1,7 +1,10 @@
 package com.yupi.aicodehelper.ai;
 
 import dev.langchain4j.service.SystemMessage;
+import dev.langchain4j.service.spring.AiService;
 
+// Springboot 为 @AiService 生成代理对象
+//@AiService
 public interface AiCodeHelperService {
 
     @SystemMessage(fromResource = "system-prompt.txt")
